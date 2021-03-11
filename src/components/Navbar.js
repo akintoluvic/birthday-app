01a75a88986468cb83b9f201b5e6c9d7d22db6f1
Vick Greenfields <akintoluvic@users.noreply.github.com>
2021-03-11 20:38:15 +0100

    fix home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <header className="fixed w-full bg-white text-gray-600 ">
+        <header className="fixed w-full bg-white text-gray-800 ">
             <div className="container flex justify-between py-5 items-center">
                 <Link to="/" className="flex title-font font-medium items-center">
-                    <span className="p-2 rounded-full bg-gray-500 text-white">
+                    <span className="p-2 rounded-full border-2 border-blue-500 text-blue-600">
                         <svg className="h-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path fillRule="evenodd" d="M6 3a1 1 0 011-1h.01a1 1 0 010 2H7a1 1 0 01-1-1zm2 3a1 1 0 00-2 0v1a2 2 0 00-2 2v1a2 2 0 00-2 2v.683a3.7 3.7 0 011.055.485 1.704 1.704 0 001.89 0 3.704 3.704 0 014.11 0 1.704 1.704 0 001.89 0 3.704 3.704 0 014.11 0 1.704 1.704 0 001.89 0A3.7 3.7 0 0118 12.683V12a2 2 0 00-2-2V9a2 2 0 00-2-2V6a1 1 0 10-2 0v1h-1V6a1 1 0 10-2 0v1H8V6zm10 8.868a3.704 3.704 0 01-4.055-.036 1.704 1.704 0 00-1.89 0 3.704 3.704 0 01-4.11 0 1.704 1.704 0 00-1.89 0A3.704 3.704 0 012 14.868V17a1 1 0 001 1h14a1 1 0 001-1v-2.132zM9 3a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1zm3 0a1 1 0 011-1h.01a1 1 0 110 2H13a1 1 0 01-1-1z" clipRule="evenodd" />
                         </svg>
@@ -15,9 +15,9 @@ const Navbar = () => {
                     <span className="hidden md:inline ml-3 text-xl font-semibold">Birthday App</span>
                 </Link>
                 <nav className="ml-auto flex flex-wrap items-center text-base justify-center">
-                    <Link to="/" className="mr-5 hover:text-gray-900">Home</Link>
-                    <Link to="/new" className="mr-5 hover:text-gray-900">Add New</Link>
-                    <Link to="/details" className="mr-5 hover:text-gray-900">Full Details</Link>
+                    <Link to="/" className="mr-5 hover:text-blue-700">Home</Link>
+                    <Link to="/new" className="mr-5 hover:text-blue-700">Add New</Link>
+                    <Link to="/details" className="mr-5 hover:text-blue-700">Full Details</Link>
                 </nav>
             </div>
         </header>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import PageWrapper from "../components/PageWrapper";
 
 const Home = () => {
@@ -7,10 +8,23 @@ const Home = () => {
             <header className="max-w-3xl mx-auto text-center mt-12">
                 <h1 className="text-6xl font-extrabold text-blue-800 mb-4">Seemles Tracking of Birthdays and all Events </h1> 
                 <h2 className="text-2xl text-blue-900 opacity-85 mb-16">You don't have to memorize them any more</h2> 
-                <button className="bg-blue-600 text-white font-semibold rounded-md max-w-xs w-full py-3">Track New Event</button>
+                <Link to="/new" className="bg-blue-600 text-white font-semibold rounded-md px-14 py-3">Track New Event</Link>
             </header>
-            <section className="max-w-2xl mx-auto bg-white rounded-2xl shadow-2xl p-8 mt-16">
+            <section className="max-w-2xl mx-auto bg-white rounded-2xl shadow-2xl p-8 my-16">
                 <h2 className="text-2xl font-semibold text-gray-600 mt-2 mb-5 text-center">Tracked Events</h2> 
+                <div className="flex justify-between bg-gray-100 border-b items-center my-5 py-2 px-2">
+                    <h3 className="w-2/5">Asante John</h3>
+                    <span>Action</span>
+                </div>
+                <div className="flex justify-between items-center my-5 px-2">
+                    <h3 className="w-2/5">Asante John</h3>
+                    <button className="bg-blue-600 text-white text-sm font-semibold rounded-md px-5 py-2">View Details</button>
+                </div>
+                <hr />
+                <div className="flex justify-between items-center my-5 px-2">
+                    <h3 className="w-2/5">Asante John</h3>
+                    <button className="bg-blue-600 text-white text-sm font-semibold rounded-md px-5 py-2">View Details</button>
+                </div>
                 <hr />
                 <div className="flex justify-between items-center my-5 px-2">
                     <h3 className="w-2/5">Asante John</h3>
